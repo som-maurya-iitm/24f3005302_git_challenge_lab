e2f4c6d6bf244cdcc7a4ec53d8bf7d99150c91cd
som-maurya-iitm <24f3005302@ds.study.iitm.ac.in>
2025-09-16 20:43:48 +0530

    Add a program loop and quit cmd

--- main.py
+++ main.py
@@ -9,11 +9,21 @@ def greet() -> None:
     user_name = os.getlogin()
     print("""########## Calculator ##########""")
     print("#", f"Hello {user_name}! 👋".center(27), "#")
+    print("#", " "*28, "#")
+    print("#", "*** Commands: ***", "#".rjust(12))
+    print("#", f"\tQuit: {bold('q')}", "#".rjust(20))
     print("#" * 32)
 
 
 def main() -> None:
     greet()
+    while True:
+        user_input = input("> ")
+        if user_input == "q":
+            break
+        else:
+            print("Unknown Command!")
+
 
 
 if __name__ == "__main__":
